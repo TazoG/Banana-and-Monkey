e641502ff0c27c9a8fd7747f32848ffeb7988f95
Tazo <tazogigitashvili@gmail.com>
2023-02-28 19:03:49 +0400

    Created banana and monkey imageviews

--- Exercise25 - Gestures/ViewController.swift
+++ Exercise25 - Gestures/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bananaView: UIImageView!
+    @IBOutlet weak var monkeyView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
